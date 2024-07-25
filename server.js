8050998f77561e82e355ecf00d1d824373b571cb
georgelucgo <georgelucgo@gmail.com>
2024-07-25 10:46:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-import jsonServer from 'json-server';
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('/db.json');
+const router = jsonServer.router('api/db.json'); 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}));
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server esta rodando');
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
 });
